5717cb17e41340c669923ec0a18231f7c5c7b823
Lokiwang <wangjian1009@gmail.com>
2012-06-16 15:10:27 +0800

    move otm from gd to cpe

--- src/gd/otm/config.mk
+++
@@ -1,6 +0,0 @@
-product:=gd_otm
-$(product).type:=lib
-$(product).depends:=cpe_utils
-$(product).c.sources:=$(wildcard $(product-base)/*.c)
-
-$(eval $(call product-def,$(product)))
--- src/gd/otm/otm_internal_ops.h
+++
@@ -1,9 +0,0 @@
-#ifndef GD_OTM_INTERNAL_OPS_H
-#define GD_OTM_INTERNAL_OPS_H
-#include "otm_internal_types.h"
-
-uint32_t otm_timer_hash(const struct otm_timer * timer);
-int otm_timer_cmp(const struct otm_timer * l, const struct otm_timer * r);
-void otm_timer_free_all(otm_manage_t mgr);
-
-#endif
