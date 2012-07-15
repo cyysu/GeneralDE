03ddc58a5bc630dd305a24518e40c0ba9321db11
Lokiwang <wangjian1009@gmail.com>
2012-07-15 16:29:57 +0800

    将gd/om系列移动到cpe/pom

--- src/gd/om_grp/tests-env/config.mk
+++
@@ -1,7 +0,0 @@
-product:=testenv.gd_om_grp
-$(product).type:=lib
-$(product).buildfor:=dev
-$(product).depends:=testenv.utils testenv.cpe_cfg testenv.cpe_dr 	gd_om_grp
-$(product).c.sources:=$(wildcard $(product-base)/*.cpp)
-
-$(eval $(call product-def,$(product)))
