d04127e94ea65d61ade7c5df1d4bd1ca31bbe352
Lokiwang <wangjian1009@gmail.com>
2012-07-27 22:23:58 +0800

    utils.bitarry计算长度的一个Bug修复

--- include/cpe/utils/bitarry.h
+++ include/cpe/utils/bitarry.h
@@ -29,7 +29,7 @@ ssize_t cpe_ba_next_pos(cpe_ba_t ba, size_t bit_capacity);
 
 /*
   size_t cpe_ba_bytes_from_bits(size_t bit_capacity); */
-#define cpe_ba_bytes_from_bits(nbits) ((size_t)((nbits + 7) >> 3))
+size_t cpe_ba_bytes_from_bits(size_t nbits);
 #define cpe_ba_bits_from_bytes(nbits) ((size_t)(nbits << 3))
 
 /*
