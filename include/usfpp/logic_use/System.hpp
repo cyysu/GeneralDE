3ea96f0a67cf4e3d94b7964b7b4609e33aaf52c8
Lokiwang <wangjian1009@gmail.com>
2012-07-31 08:39:03 +0800

    logic_use增加几个辅助类，用于构造变长数据

--- include/usfpp/logic_use/System.hpp
+++ include/usfpp/logic_use/System.hpp
@@ -1,10 +1,18 @@
 #ifndef USFPP_LOGIC_USE_SYSTEM_H
 #define USFPP_LOGIC_USE_SYSTEM_H
+#include "cpepp/dr/System.hpp"
 #include "usf/logic_use/logic_use_types.h"
+#include "usfpp/logic/System.hpp"
 
 namespace Usf { namespace Logic {
 
 class LogicAsyncOp;
+
+template<typename ListT, typename EleT>
+class LogicOpDynList;
+
+template<typename DataT, int count = Cpe::Dr::MetaTraits<DataT>::dyn_count>
+class LogicOpDynData;
 
 }}
 
