6786cdb71f6e0a9786afeea699b81ba790d9497b
Lokiwang <wangjian1009@gmail.com>
2012-08-06 18:01:03 +0800

    usf.pom_gs修改测试框架

--- src/usf/pom_gs/tests-ut/PomGsAgentTest.hpp
+++ src/usf/pom_gs/tests-ut/PomGsAgentTest.hpp
@@ -4,14 +4,16 @@
 #include "cpe/utils/tests-env/test-fixture.hpp"
 #include "cpe/utils/tests-env/with_em.hpp"
 #include "cpe/cfg/tests-env/with_cfg.hpp"
+#include "cpe/dr/tests-env/with_dr.hpp"
 #include "cpe/pom_grp/tests-env/with_pom_grp.hpp"
 #include "gd/app/tests-env/with_app.hpp"
 #include "usf/pom_gs/tests-env/with_pom_gs.hpp"
 
-typedef LOKI_TYPELIST_5(
+typedef LOKI_TYPELIST_6(
     utils::testenv::with_em
     , gd::app::testenv::with_app
     , cpe::cfg::testenv::with_cfg
+    , cpe::dr::testenv::with_dr
     , cpe::pom_grp::testenv::with_pom_grp
     , usf::pom_gs::testenv::with_pom_gs
     ) PomGsAgentTestBase;
--- src/usf/pom_gs/tests-ut/test_pom_gs_agent_basic.cpp
+++ src/usf/pom_gs/tests-ut/test_pom_gs_agent_basic.cpp
@@ -1,4 +1,31 @@
 #include "PomGsAgentTest.hpp"
 
-TEST_F(PomGsAgentTest, defaults) {
+class PomGsAgentBasicTest : public PomGsAgentTest {
+public:
+    void SetUp() {
+        PomGsAgentTest::SetUp();
+
+        installAgent(
+            "TestObj:\n"
+            "  - entry1: { entry-type: normal, data-type: AttrGroup1 }\n"
+            "  - entry2: { entry-type: list, data-type: AttrGroup2, group-count: 3, capacity: 3 }\n"
+            "  - entry3: { entry-type: ba, bit-capacity: 15 }\n"
+            "  - entry4: { entry-type: binary, capacity: 5 }\n"
+            ,
+            "<metalib tagsetversion='1' name='net'  version='1'>"
+            "    <struct name='AttrGroup1' version='1'>"
+            "	     <entry name='a1' type='uint32' id='1'/>"
+            "    </struct>"
+            "    <struct name='AttrGroup2' version='1'>"
+            "	     <entry name='b1' type='string' size='5' id='1'/>"
+            "    </struct>"
+            "</metalib>"
+            ,
+            "entry1"
+            ,
+            "a1");
+    }
+};
+
+TEST_F(PomGsAgentBasicTest, basic) {
 }
