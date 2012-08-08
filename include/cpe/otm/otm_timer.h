14cde3521f5b8411ee18879358d6e16727c5a87d
Lokiwang <wangjian1009@gmail.com>
2012-08-08 08:35:09 +0800

    omt增加is_enable的判断

--- include/cpe/otm/otm_timer.h
+++ include/cpe/otm/otm_timer.h
@@ -29,6 +29,8 @@ int otm_timer_auto_enable(otm_timer_t timer);
 void otm_timer_enable(otm_timer_t timer, uint32_t cur_time_s, uint32_t first_exec_span_s, otm_memo_t memo);
 void otm_timer_disable(otm_timer_t timer, otm_memo_t memo);
 
+int otm_timer_is_enable(otm_timer_t timer, otm_memo_t memo);
+
 #ifdef __cplusplus
 }
 #endif
