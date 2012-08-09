4abf77ba98a4494533321111b40206346d947f26
Lokiwang <wangjian1009@gmail.com>
2012-08-09 18:55:44 +0800

    gd增加vnet模块，定义了通用的控制协议

---
+++ src/gd/vnet/config.mk
@@ -0,0 +1,10 @@
+product:=gd_vnet
+$(product).type:=lib
+$(product).depends:=cpe_utils gd_app
+$(product).c.flags.ld:=
+$(product).c.sources:=$(wildcard $(product-base)/*.c)
+
+$(eval $(call product-def,$(product)))
+
+
+
