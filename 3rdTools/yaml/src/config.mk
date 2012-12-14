e91ab355432fd76736f59b03bbf55ba524daf303
Lokiwang <wangjian1009@gmail.com>
2012-12-14 14:49:55 +0800

    更新编译设置

--- 3rdTools/yaml/src/config.mk
+++ 3rdTools/yaml/src/config.mk
@@ -4,7 +4,7 @@ $(product).product.c.includes:=3rdTools/yaml/include
 $(product).c.sources:=$(wildcard $(product-base)/*.c)
 $(product).c.env-includes:=3rdTools/yaml/src
 $(product).c.flags.cpp:=-DHAVE_CONFIG_H
-$(product).c.flags.lan.c:= -O2 -Wno-parentheses-equality
+$(product).c.flags.lan.c:= -O2 -Wno-parentheses-equality -Wno-unused-but-set-variable -Wno-unused-value
 $(product).c.flags.ld:=
 
 $(eval $(call product-def,$(product)))
