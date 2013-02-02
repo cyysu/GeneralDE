e56bce076adb08863e1c4b46c8ce5c023799c5fb
Lokiwang <wangjian1009@gmail.com>
2013-02-02 12:31:22 +0800

    好友相关代码更新

--- src/svr/friend/agent/src/FriendSvr_Op_Query.cpp
+++ src/svr/friend/agent/src/FriendSvr_Op_Query.cpp
@@ -2,13 +2,18 @@
 #include "cpepp/cfg/Node.hpp"
 #include "cpepp/tl/Manager.hpp"
 #include "usfpp/logic/LogicOp.hpp"
+#include "usfpp/logic/LogicOpRequire.hpp"
+#include "usfpp/logic/LogicOpStack.hpp"
 #include "usfpp/logic/LogicOpDef.hpp"
-#include "usfpp/logic_use/LogicOpDynDataT.hpp"
+#include "usfpp/mongo_driver/Package.hpp"
+#include "usfpp/mongo_cli/CliProxy.hpp"
+#include "usfpp/mongo_cli/Result.hpp"
 #include "FriendSvrLogicOp.hpp"
+#include "FriendSvrLogic.hpp"
 
 namespace Svr { namespace Friend {
 
-class FriendSvr_Op_Query : public LogicOp {
+class FriendSvr_Op_Query : public LogicAsyncOp {
 public:
     static cpe_hash_string_t NAME;
 
@@ -19,9 +24,19 @@ public:
     {
     }
 
-    virtual R execute(
-        LogicOpContext & context, 
+
+    virtual R send(
+        LogicOpContext & context,
         Usf::Logic::LogicOpStackNode & stackNode,
+        Cpe::Cfg::Node const & args) const
+    {
+        return true;
+    }
+
+    virtual R recv(
+        LogicOpContext & context,
+        Usf::Logic::LogicOpStackNode & stackNode,
+        Usf::Logic::LogicOpRequire & require,
         Cpe::Cfg::Node const & args) const
     {
         return true;
