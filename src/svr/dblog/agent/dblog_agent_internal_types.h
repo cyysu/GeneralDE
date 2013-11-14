39c985f8e0cbfdbdb74ace00cb5951953eb17129
wangjian1009 <wangjian1009@gmail.coom>
2013-11-14 09:07:14 +0800

    svr.dblog补充代码，未完成

--- src/svr/dblog/agent/dblog_agent_internal_types.h
+++ src/svr/dblog/agent/dblog_agent_internal_types.h
@@ -1,5 +1,5 @@
-#ifndef SVR_DBLOG_AGENT_TYPES_H
-#define SVR_DBLOG_AGENT_TYPES_H
+#ifndef SVR_DBLOG_AGENT_INTERNAL_TYPES_H
+#define SVR_DBLOG_AGENT_INTERNAL_TYPES_H
 #include "cpe/utils/memory.h"
 #include "cpe/utils/error.h"
 #include "cpe/pal/pal_socket.h"
@@ -8,11 +8,12 @@
 #include "svr/set/stub/set_svr_stub_types.h"
 #include "svr/dblog/agent/dblog_agent_types.h"
 
-struct dblog_svr {
+struct dblog_agent {
     gd_app_context_t m_app;
     mem_allocrator_t m_alloc;
     error_monitor_t m_em;
     set_svr_stub_t m_stub;
+    uint16_t m_dblog_svr_type_id;
     int m_debug;
 };
 
