2d0bc03d1071b8d7c57fc7209ee73272e9defb87
wangjian1009 <wangjian1009@gmail.coom>
2013-12-03 15:45:14 +0800

    friend更新

---
+++ src/svr/friend/svr/src/friend_svr_db_ops.c
@@ -0,0 +1,127 @@
+#include "cpe/pal/pal_stdio.h"
+#include "cpe/dr/dr_metalib_manage.h"
+#include "cpe/tl/tl_manage.h"
+#include "gd/app/app_context.h"
+#include "usf/logic/logic_data.h"
+#include "usf/logic/logic_require.h"
+#include "usf/mongo_driver/mongo_pkg.h"
+#include "friend_svr_ops.h"
+
+int friend_svr_db_send_query(friend_svr_t svr, logic_require_t require, uint64_t user_id) {
+    int pkg_r;
+    mongo_pkg_t db_pkg;
+
+    db_pkg = mongo_cli_proxy_pkg_buf(svr->m_db);
+    if (db_pkg == NULL) {
+        CPE_ERROR(
+            svr->m_em, "%s: %s: get db pkg fail!",
+            friend_svr_name(svr), logic_require_name(require));
+        return -1;
+    }
+
+    mongo_pkg_init(db_pkg);
+    mongo_pkg_set_collection(db_pkg, "friend_data");
+    mongo_pkg_set_op(db_pkg, mongo_db_op_query);
+
+    pkg_r = 0;
+
+    pkg_r |= mongo_pkg_doc_open(db_pkg);
+    pkg_r |= mongo_pkg_append_int64(db_pkg, "user_id", (int64_t)user_id);
+    pkg_r |=  mongo_pkg_doc_close(db_pkg);
+
+    /*需要的列 */
+    pkg_r |= mongo_pkg_doc_open(db_pkg);
+    pkg_r |= mongo_pkg_append_int32(db_pkg, dr_entry_name(svr->m_record_fuid_entry), 1);
+    pkg_r |= mongo_pkg_doc_close(db_pkg);
+
+    if (pkg_r) {
+        CPE_ERROR(
+            svr->m_em, "%s: %s: build query req fail!",
+            friend_svr_name(svr), logic_require_name(require));
+        return -1;
+    }
+
+    if (mongo_cli_proxy_send(svr->m_db, db_pkg, require, svr->m_record_list_meta, 32, NULL) != 0) {
+        CPE_ERROR(
+            svr->m_em, "%s: %s: send db request fail!",
+            friend_svr_name(svr), logic_require_name(require));
+        return -1;
+    }
+
+    return 0;
+}
+
+int friend_svr_db_send_insert(friend_svr_t svr, logic_require_t require, void const * record) {
+    mongo_pkg_t db_pkg;
+    int pkg_r;
+
+    db_pkg = mongo_cli_proxy_pkg_buf(svr->m_db);
+    if (db_pkg == NULL) {
+        CPE_ERROR(svr->m_em, "%s: %s:: get db pkg fail!", friend_svr_name(svr), logic_require_name(require));
+        return -1;
+    }
+
+    mongo_pkg_init(db_pkg);
+    mongo_pkg_set_collection(db_pkg, "friend_data");
+    mongo_pkg_set_op(db_pkg, mongo_db_op_insert);
+
+    pkg_r = 0;
+    pkg_r |= mongo_pkg_doc_append(db_pkg, svr->m_record_meta, record, svr->m_record_size);
+
+    if (pkg_r) {
+        CPE_ERROR(
+            svr->m_em, "%s: %s: build insert req fail!",
+            friend_svr_name(svr), logic_require_name(require));
+        return -1;
+    }
+
+    if (mongo_cli_proxy_send(svr->m_db, db_pkg, require, svr->m_record_list_meta, 0, NULL) != 0) {
+        CPE_ERROR(
+            svr->m_em, "%s: %s: send db request fail!",
+            friend_svr_name(svr), logic_require_name(require));
+        return -1;
+    }
+
+    return 0;
+}
+
+int friend_svr_db_send_remove(friend_svr_t svr, logic_require_t require, uint64_t uid, uint64_t fuid) {
+    mongo_pkg_t db_pkg;
+    int pkg_r;
+    char buf[64];
+
+    db_pkg = mongo_cli_proxy_pkg_buf(svr->m_db);
+    if (db_pkg == NULL) {
+        CPE_ERROR(svr->m_em, "%s: %s:: get db pkg fail!", friend_svr_name(svr), logic_require_name(require));
+        return -1;
+    }
+
+    mongo_pkg_init(db_pkg);
+    mongo_pkg_set_collection(db_pkg, "friend_data");
+    mongo_pkg_set_op(db_pkg, mongo_db_op_delete);
+
+    pkg_r = 0;
+
+    pkg_r |= mongo_pkg_doc_open(db_pkg);
+    snprintf(buf, sizeof(buf), FMT_UINT64_T"-"FMT_UINT64_T, uid, fuid);
+    pkg_r |= mongo_pkg_append_string(db_pkg, "_id", buf);
+    pkg_r |=  mongo_pkg_doc_close(db_pkg);
+
+    if (pkg_r) {
+        CPE_ERROR(
+            svr->m_em, "%s: %s: build delete req fail!",
+            friend_svr_name(svr), logic_require_name(require));
+        return -1;
+    }
+
+    if (mongo_cli_proxy_send(svr->m_db, db_pkg, require, svr->m_record_list_meta, 0, NULL) != 0) {
+        CPE_ERROR(
+            svr->m_em, "%s: %s: send db request fail!",
+            friend_svr_name(svr), logic_require_name(require));
+        return -1;
+    }
+
+    return 0;
+}
+
+
--- src/svr/friend/svr/src/friend_svr_op_add.c
+++ src/svr/friend/svr/src/friend_svr_op_add.c
@@ -13,6 +13,63 @@ logic_op_exec_result_t
 friend_svr_op_add_send(
     logic_context_t ctx, logic_stack_node_t stack, void * user_data, cfg_t cfg)
 {
+<<<<<<< Updated upstream
+=======
+    friend_svr_t svr = user_data;
+    logic_require_t require;
+    logic_data_t req_data;
+    SVR_FRIEND_REQ_ADD const * req;
+    int rv;
+    char buf[svr->m_record_size];
+
+    req_data = logic_context_data_find(ctx, "svr_friend_req_add");
+    if (req_data == NULL) {
+        APP_CTX_ERROR(logic_context_app(ctx), "%s: add: get request fail!", friend_svr_name(svr));
+        logic_context_errno_set(ctx, SVR_FRIEND_ERRNO_INTERNAL);
+        return logic_op_exec_result_false;
+    }
+    req = logic_data_data(req_data);
+
+    bzero(buf, sizeof(buf));
+
+    rv = dr_pbuf_read(
+        buf + svr->m_record_data_start_pos,
+        sizeof(buf) - svr->m_record_data_start_pos,
+        req->data,
+        req->data_len,
+        svr->m_data_meta,
+        svr->m_em);
+    if (rv <= 0) {
+        APP_CTX_ERROR(logic_context_app(ctx), "%s: add: decode data error, rv=%d!", friend_svr_name(svr), rv);
+        logic_context_errno_set(ctx, SVR_FRIEND_ERRNO_INTERNAL);
+        return logic_op_exec_result_false;
+    }
+
+    if (dr_entry_set_from_uint64(buf + svr->m_record_uid_start_pos, req->user_id, svr->m_record_uid_entry, svr->m_em) != 0) {
+        APP_CTX_ERROR(logic_context_app(ctx), "%s: add: set uid error!", friend_svr_name(svr));
+        logic_context_errno_set(ctx, SVR_FRIEND_ERRNO_INTERNAL);
+        return logic_op_exec_result_false;
+    }
+
+    if (friend_svr_set_record_id(svr, buf) != 0) {
+        APP_CTX_ERROR(logic_context_app(ctx), "%s: add: set _id error!", friend_svr_name(svr));
+        logic_context_errno_set(ctx, SVR_FRIEND_ERRNO_INTERNAL);
+        return logic_op_exec_result_false;
+    }
+
+    require = logic_require_create(stack, "insert");
+    if (require == NULL) {
+        APP_CTX_ERROR(logic_context_app(ctx), "%s: add: create logic require fail!", friend_svr_name(svr));
+        logic_context_errno_set(ctx, SVR_FRIEND_ERRNO_INTERNAL);
+        return logic_op_exec_result_false;
+    }
+
+    if (friend_svr_db_send_insert(svr, require, buf) != 0) {
+        logic_context_errno_set(ctx, SVR_FRIEND_ERRNO_INTERNAL);
+        return logic_op_exec_result_false;
+    }
+
+>>>>>>> Stashed changes
     return logic_op_exec_result_true;
 }
 
@@ -21,5 +78,38 @@ friend_svr_op_add_recv(
     logic_context_t ctx, logic_stack_node_t stack, logic_require_t require,
     void * user_data, cfg_t cfg)
 {
+<<<<<<< Updated upstream
+=======
+    friend_svr_t svr = user_data;
+
+    if (logic_require_state(require) != logic_require_state_done) {
+        if (logic_require_state(require) != logic_require_state_error) {
+            APP_CTX_ERROR(
+                logic_context_app(ctx), "%s: add: db request state error, state=%s!",
+                friend_svr_name(svr), logic_require_state_name(logic_require_state(require)));
+            logic_context_errno_set(ctx, SVR_FRIEND_ERRNO_INTERNAL);
+            return logic_op_exec_result_false;
+        }
+        else if (logic_require_error(require) == mongo_data_error_duplicate_key) {
+            if (svr->m_debug) {
+                APP_CTX_INFO(
+                    logic_context_app(ctx), "%s: add: friend already exist!",
+                    friend_svr_name(svr));
+            }
+
+            logic_context_errno_set(ctx, SVR_FRIEND_ERRNO_ALREADY_EXIST);
+
+            return logic_op_exec_result_false;
+        }
+        else {
+            APP_CTX_ERROR(
+                logic_context_app(ctx), "%s: add: db request error, errno=%d!",
+                friend_svr_name(svr), logic_require_error(require));
+            logic_context_errno_set(ctx, SVR_FRIEND_ERRNO_DB);
+            return logic_op_exec_result_false;
+        }
+    }
+
+>>>>>>> Stashed changes
     return logic_op_exec_result_true;
 }
