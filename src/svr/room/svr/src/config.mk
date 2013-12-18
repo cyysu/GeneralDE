92f8d9b596259111b88ff6baaccaf5dba2ef674c
wangjian1009 <wangjian1009@gmail.coom>
2013-12-18 20:19:28 +0800

    merge

--- src/svr/room/svr/src/config.mk
+++ src/svr/room/svr/src/config.mk
@@ -1,4 +1,4 @@
-product:=chat_svr_lib
+product:=room_svr_lib
 $(product).type:=cpe-dr lib 
 $(product).depends:=cpe_cfg cpe_dr cpe_dr_data_cfg cpe_dr_data_pbuf cpe_tl cpe_aom cpe_dp cpe_nm \
                     gd_app  gd_dr_store gd_timer gd_log \
@@ -13,24 +13,30 @@ $(product).cpe-dr.modules:=pro pro-use
 #编译data协议定义
 $(product).cpe-dr.pro.generate:=h c
 $(product).cpe-dr.pro.source:=$(addprefix $(product-base)/../pro/, \
-                                   cli/svr_chat_pro.xml \
-                                   svr/svr_chat_meta.xml svr/svr_chat_internal.xml)
-$(product).cpe-dr.pro.h.output:=protocol/svr/chat
-$(product).cpe-dr.pro.c.output:=protocol/svr/chat/metalib.c
-$(product).cpe-dr.pro.c.arg-name:=g_metalib_svr_chat_pro
+                                   cli/svr_room_data.xml cli/svr_room_pro.xml \
+                                   svr/svr_room_meta.xml svr/svr_room_internal.xml)
+$(product).cpe-dr.pro.h.output:=protocol/svr/room
+$(product).cpe-dr.pro.c.output:=protocol/svr/room/metalib.c
+$(product).cpe-dr.pro.c.arg-name:=g_metalib_svr_room_pro
 
 $(product).cpe-dr.pro-use.generate:=bin
 $(product).cpe-dr.pro-use.source:=$(addprefix $(product-base)/../pro/, \
-                                     cli/svr_chat_pro.xml )
-$(product).cpe-dr.pro-use.bin.output:=meta/chat_svr.dr
+                                     cli/svr_room_data.xml cli/svr_room_pro.xml )
+$(product).cpe-dr.pro-use.bin.output:=meta/room_svr.dr
 
 $(eval $(call product-def,$(product)))
 
-product:=chat_svr
+product:=room_svr
 $(product).type:=progn
-$(product).depends:=chat_svr_lib argtable2
+$(product).depends:=room_svr_lib argtable2
 $(product).c.sources:=$(product-base)/main.c
 $(product).c.flags.ld:=-lm -rdynamic
 $(product).c.export-symbols:=$(patsubst _%,%,$(shell cat $(product-base)/symbols.def))
 
 $(eval $(call product-def,$(product)))
+
+.DELETE_ON_ERROR: $(product-base)/symbols.vc.def
+$(product-base)/symbols.vc.def: $(product-base)/symbols.def
+	$(CPDE_PERL) $(CPDE_ROOT)/buildtools/tools/gen-vc-symbols.pl --input-file $< --output-file $@
+
+room_svr: $(product-base)/symbols.vc.def
