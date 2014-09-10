ff3a3c0e7cda7c8b691edbd94fb699f9d7d7d018
wangjian1009 <wangjian1009@gmail.coom>
2014-09-10 22:11:41 +0800

    update ui

--- include/uipp/sprite_b2/B2System.hpp
+++ include/uipp/sprite_b2/B2System.hpp
@@ -7,9 +7,22 @@
 
 namespace UI { namespace Sprite { namespace B2 {
 
+enum RuningMode {
+    RUNINGMODE_PASSIVE = UI_SPRITE_B2_OBJ_RUNING_MODE_PASSIVE,
+    RUNINGMODE_ACTIVE = UI_SPRITE_B2_OBJ_RUNING_MODE_ACTIVE
+};
+
+enum ObjectType {
+	OBJECTTYPE_STATIC = UI_SPRITE_B2_OBJ_TYPE_STATIC,
+	OBJECTTYPE_KINEMATIC = UI_SPRITE_B2_OBJ_TYPE_KINEMATIC,
+    OBJECTTYPE_DYNAMIC = UI_SPRITE_B2_OBJ_TYPE_DYNAMIC,
+};
+
 class B2System;
 class B2Object;
 class B2ObjectPart;
+
+extern P2D::Pair const V_ZERO;
 
 }}}
 
