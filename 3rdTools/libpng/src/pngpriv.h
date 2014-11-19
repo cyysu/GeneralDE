943648ac2f1d3a400a885183c311862e0281d103
wangjian1009 <wangjian1009@gmail.coom>
2014-11-19 18:21:43 +0800

    update pngpriv

--- 3rdTools/libpng/src/pngpriv.h
+++ 3rdTools/libpng/src/pngpriv.h
@@ -283,7 +283,7 @@ typedef PNG_CONST png_uint_16p FAR * png_const_uint_16pp;
 #  include <float.h>
 
 #  if (defined(__MWERKS__) && defined(macintosh)) || defined(applec) || \
-    defined(THINK_C) || defined(__SC__) || defined(TARGET_OS_MAC)
+    defined(THINK_C) || defined(__SC__)
      /* We need to check that <math.h> hasn't already been included earlier
       * as it seems it doesn't agree with <fp.h>, yet we should really use
       * <fp.h> if possible.
