90e5c448e41738326839adffadb230d960b27297
wangjian1009 <wangjian1009@gmail.coom>
2014-11-24 16:15:07 +0800

    remove sprite_barrate sprite_spine

--- src/ui/sprite_spine/config.mk
+++
@@ -1,7 +0,0 @@
-product:=ui_sprite_spine
-$(product).type:=lib
-$(product).depends:=ui_sprite ui_sprite_fsm ui_spine
-$(product).c.libraries:=
-$(product).c.sources:=$(wildcard $(product-base)/*.c)
-
-$(eval $(call product-def,$(product)))
